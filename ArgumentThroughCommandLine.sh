7179da649dd39da361bb34b9f870371f13d1eba3
Nehasingh1300 <40912375+Nehasingh1300@users.noreply.github.com>
2020-01-17 10:40:56 +0530

    Create ArgumentThroughCommandLine.sh

---
+++ ArgumentThroughCommandLine.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "My first name is $1"         //prints first argument
+echo "My second name is $2"        // prints 2nd argument
+echo "total number of argument $#" // print number of arguments 
